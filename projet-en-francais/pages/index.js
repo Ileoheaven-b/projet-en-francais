0a283481278b681701be50ca134dc6ce85607c84
Ileoheaven-b <ileoheaven.b@gmail.com>
2023-01-17 19:49:51 +0200

    Default files

---
+++ projet-en-francais/pages/index.js
@@ -0,0 +1,11 @@
+import styles from "../styles/index.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <div className={styles.homeContainer}>
+        <div className={styles.home}>TBC</div>
+      </div>
+    </>
+  );
+}
