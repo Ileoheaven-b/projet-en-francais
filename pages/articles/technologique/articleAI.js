42a6bf96475f1f81470c7d36e7ebe65919098df0
distroierONE <serban.matei.gireada@gmail.com>
2023-01-25 00:12:01 +0200

    Added template for articles - WIP Added other QOL Changes

---
+++ pages/articles/technologique/articleAI.js
@@ -0,0 +1,26 @@
+import styles from "../../../styles/article.module.css";
+import Link from "next/link";
+
+export default function Informations() {
+  return (
+    <>
+      <div className={styles.homeContainer}>
+        <div className={styles.textBox}>
+          <div className={styles.title}>WORK IN PROGRESS</div>
+          <div className={styles.text}>
+            Lorem ipsum dolor parte mare de calamare din marea cea tare si bate
+            vantu trece frunza Lorem ipsum dolor parte mare de calamare din
+            marea cea tare si bate vantu trece frunzaLorem ipsum dolor parte
+            mare de calamare din marea cea tare si bate vantu trece frunzaLorem
+            ipsum dolor parte mare de calamare din marea cea tare si bate vantu
+            trece frunzaLorem ipsum dolor parte mare de calamare din marea cea
+            tare si bate vantu trece frunzaLorem ipsum dolor parte mare de
+            calamare din marea cea tare si bate vantu trece frunzaLorem ipsum
+            dolor parte mare de calamare din marea cea tare si bate vantu trece
+            frunza
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import styles from "../styles/index.module.css";
 import Link from "next/link";
+import Image from "next/image";
+import intelligence from "./imagesFrontPage/ArtificialIntelligence.png";
+import placeholderImage from "./imagesFrontPage/PlaceholderImage.jpg";
 
 export default function Home() {
   return (
@@ -22,20 +25,22 @@ export default function Home() {
             </div>
           </Link>
 
-          <Link className={styles.article} href="/other">
-            <div className={styles.articleTitle}>Placeholder Title</div>
-            <div className={styles.articleText}>
-              Lorem ipsum dolor parte mare de calamare din marea cea tare si
-              bate vantu trece frunzaLorem ipsum dolor parte mare de calamare
-              din marea cea tare si bate vantu trece frunzaLorem ipsum dolor
-              parte mare de calamare din marea cea tare si bate vantu trece
-              frunzaLorem ipsum dolor parte mare de calamare din marea cea tare
-              si bate vantu trece frunzaLorem ipsum dolor parte mare de calamare
-              din marea cea tare si bate vantu trece frunzaLorem ipsum dolor
-              parte mare de calamare din marea cea tare si bate vantu trece
-              frunzaLorem ipsum dolor parte mare de calamare din marea cea tare
-              si bate vantu trece frunza
+          <Link className={styles.article} href="/articles/technologique/articleAI">
+            <div className={styles.articleTitle}>
+              ChatGPT : Le partenaire d'étude intelligent
             </div>
+            <div className={styles.articleText}>
+              ChatGPT, un outil Ai avancé qui pourrait aider les étudiants à se
+              développer dans un large éventail d'aspects de leur éducation,
+              tels que l'organisation des ressources et des informations,
+              l'écriture, la communication et le développement linguistique...
+            </div>
+            <Image
+              className={styles.articleImage}
+              src={intelligence}
+              alt="Image d'une IA"
+              layout="responsive"
+            />
           </Link>
 
           <Link className={styles.article} href="/other">
