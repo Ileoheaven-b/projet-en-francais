88a6143722dacce36abe394ebf590cc8eb2aefd5
distroierONE <serban.matei.gireada@gmail.com>
2023-01-26 01:01:13 +0200

    Added early setup for mobile support

--- components/modal/modal.js
+++ components/modal/modal.js
@@ -20,10 +20,19 @@ export default function Modal() {
           <div onClick={toggleModal} className={styles.overlay}></div>
           <div className={styles.modal_content}>
             <div className={styles.title_modal}>Libre de s'abonner</div>
+            <select className={styles.choice_modal}>
+              <option value="gratis">Gratis</option>
+              <option value="standard">Standard</option>
+              <option value="premium">Premium</option>
+            </select>
             <div className={styles.input_holder_modal}>
               <input
                 className={styles.input_modal}
                 placeholder="exemple@gmail.com"
+                type="email"
+                id="email"
+                pattern=".+@globex\.com"
+                required
               />{" "}
               <button className={styles.send_modal} onClick={toggleModal}>
                 Je m'abonne
